ba77fd02a16c19882d24c56b0defdc2dc0bfc463
KatePril <ket14pril14@ukr.net>
2024-03-18 21:36:37 +0200

    Update matrixPrinter

--- src/app/view/matrix/MatrixPrinter.java
+++ src/app/view/matrix/MatrixPrinter.java
@@ -2,11 +2,21 @@ package app.view.matrix;
 
 public class MatrixPrinter<T> {
     public void printMatrix(T[][] matrix) {
+        System.out.print(" \t\t");
+        for (int i = 1; i <= matrix.length; i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+        System.out.println();
+
+        int k = 1;
         for (T[] ints : matrix) {
+            System.out.print(k + "\t\t");
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(ints[j] + " ");
+                System.out.print(ints[j] + "\t");
             }
             System.out.println();
+            ++k;
         }
     }
 }
